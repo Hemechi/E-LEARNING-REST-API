0c34226cff12057af2376a2bdf448a99baa11c22
Hemechi <helen.leang15@gmail.com>
2024-04-18 16:36:53 +0700

    done: Authorities-Management

--- src/main/java/co/istad/elearningrestapi/features/user/UserController.java
+++ src/main/java/co/istad/elearningrestapi/features/user/UserController.java
@@ -14,23 +14,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
-
-
-        //    users end-points
-
-//[GET] /api/v1/users => Find all users in the platform (both students and instructors) by advanced options below:
-//    Sort by id
-//    Filter by username, email, nationalIdCard, phoneNumber, name, gender, and role
-//[GET] /api/v1/users/{username} => Find a user details
-//[PUT] /api/v1/users/{username}/disable => Disable a user
-//[PUT] /api/v1/users/{username}/enable => Enable a user
-//[DELETE] /api/v1/users/{username} => Permanently delete a user from platform
-
-//    roles end-points
-
-//[GET] /api/v1/roles => Find all roles (response with authorities)
-//[GET]/api/v1/roles/{name} => Find a role by name (response with authorities)
-
     private final UserService userService;
 
     @GetMapping
